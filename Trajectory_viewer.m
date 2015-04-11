59ebbd11c2fbbb2a0ff1a965b2db7bbc3ba2f496
Kuba Streit <kuba.streit@gmail.com>
2015-04-11 09:16:04 +0200

    added trajectory plot add_point method time span: t = 6700 + 6.8 * s [us], where s is a number of points in trajectory

--- Trajectory_viewer.m
+++ Trajectory_viewer.m
@@ -1,7 +1,9 @@
 classdef Trajectory_viewer < handle
     
     properties (Constant)
-        settings_file = 'settings.mat'
+        settings_file = 'settings.mat';
+        init_alloc_size = 2^20;
+        
     end
     
     properties (SetAccess = immutable)%, GetAccess = private)
@@ -25,7 +27,8 @@ classdef Trajectory_viewer < handle
             parser.KeepUnmatched = true;
             parse(parser, varargin{:});
             cat_params = parse_categories(parser.Unmatched, {'gui'});
-            obj.trajectory = struct('X', [], 'Y', [], 'Z', [], 't', []);
+            obj.trajectory = struct('coor', NaN(obj.init_alloc_size, 3), 'length', 1);
+            obj.trajectory.coor(1, :) = [0 0 0];
             obj.robot = struct('X', 0, 'Y', 0, 'Z', 0, 'roll', 0, 'pitch', 0, 'yaw', 0);
             cat_params.gui.DeleteFcn = @obj.delete;
             obj.gui = Gui(cat_params.gui);
@@ -67,6 +70,39 @@ classdef Trajectory_viewer < handle
                 obj.connected = true;
             end
             obj.gui.connection_sig(obj.connected);
+        end
+        
+        function add_point(obj, coor, absolute, robot_orientation)
+            if length(coor) < 3
+                coor = [coor, 0];
+            end
+            if size(obj.trajectory.coor, 1) == obj.trajectory.length
+                obj.trajectory.coor = [obj.trajectory.coor; Nan(size(obj.trajectory.coor))];
+            end
+            obj.trajectory.length = obj.trajectory.length + 1;
+            if ~(nargin < 3 || absolute)
+                coor = coor + obj.trajectory.coor(obj.trajectory.length - 1, :);
+            end
+            obj.trajectory.coor(obj.trajectory.length, :) = coor;
+            if nargin < 4 || isempty(robot_orientation)
+                l = obj.trajectory.length;
+                pitch = atan2(0, diff(obj.trajectory.coor(l-1:l, 3)));
+                yaw = atan2(diff(obj.trajectory.coor(l-1:l, 2)), ...
+                    diff(obj.trajectory.coor(l-1:l, 1)));
+                robot_orientation = struct('roll', 0, 'pitch', pitch, 'yaw', yaw);
+            end
+            obj.robot.X = coor(1);
+            obj.robot.Y = coor(2);
+            obj.robot.Z = coor(3);
+            obj.robot.roll = robot_orientation.roll;
+            obj.robot.pitch = robot_orientation.pitch;
+            obj.robot.yaw = robot_orientation.yaw;
+            set(obj.gui.h_trajectory, ...
+                'XData', obj.trajectory.coor(1:obj.trajectory.length, 1), ...
+                'YData', obj.trajectory.coor(1:obj.trajectory.length, 2), ...
+                'ZData', obj.trajectory.coor(1:obj.trajectory.length, 3));
+            obj.gui.set_robot_position(obj.robot);
+            drawnow;
         end
     end
 end
